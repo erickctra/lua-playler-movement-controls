079beb653305bdc11d9e46a753294b340d8b39ac
Erick Cintra <developer.erickcintra@outlook.com>
2022-11-14 03:12:30 -0300

    add speed property and fps counter

--- main.lua
+++ main.lua
@@ -1,12 +1,29 @@
 function love.load()
     player = {}
-    player.x = 0
-    player.y = 0
+    player.x = 400
+    player.y = 200
+    player.speed = 6
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+
+    if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+
+    if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end
 end
 
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, 100)
-    end
+    love.graphics.circle("fill", player.x, player.y, 40)
+    love.graphics.print("Current FPS: "..tostring(love.timer.getFPS( )), 10, 10)
+end
