ed119cd4e8bef364107939208039da98fd64720d
Erick Cintra <developer.erickcintra@outlook.com>
2022-11-14 02:58:28 -0300

    add player to screen

---
+++ main.lua
@@ -0,0 +1,12 @@
+function love.load()
+    player = {}
+    player.x = 0
+    player.y = 0
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.circle("fill", player.x, player.y, 100)
+    end
