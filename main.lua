2c76055d61560d58c5028361e3ea0bf56550090b
Erick Cintra <developer.erickcintra@outlook.com>
2022-11-14 04:35:35 -0300

    fix movement

--- main.lua
+++ main.lua
@@ -11,17 +11,14 @@ end
 function love.update(dt)
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed
-    end
-
-    if love.keyboard.isDown("left") then
+    
+    elseif love.keyboard.isDown("left") then
         player.x = player.x - player.speed
-    end
-
-    if love.keyboard.isDown("down") then
+    
+    elseif love.keyboard.isDown("down") then
         player.y = player.y + player.speed
-    end
-
-    if love.keyboard.isDown("up") then
+    
+    elseif love.keyboard.isDown("up") then
         player.y = player.y - player.speed
     end
 end
